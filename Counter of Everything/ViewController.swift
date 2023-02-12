8b626f6bf606ff1cac19bc8ce14e5651a5ae5777
LYNXhfdd <my@sfogel.ru>
2023-02-12 14:53:32 +0300

    Удалены лишние комментарии и лишний код

--- Counter of Everything/ViewController.swift
+++ Counter of Everything/ViewController.swift
@@ -1,24 +1,12 @@
-//
-//  ViewController.swift
-//  Counter of Everything
-//
-//  Created by Sergey Fogel on 11.02.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counterNumber: UILabel!
+    @IBOutlet weak private var counterNumber: UILabel!
     
-    var count = 0
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    private var count = 0
     
-    @IBAction func buttonTapped(_ sender: Any) {
+    @IBAction private func buttonTapped(_ sender: Any) {
         count += 1
         counterNumber.text = String(count)
     }
