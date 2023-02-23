e884cc502139131c2e1b33703e0b5ad027b40b0c
LYNXhfdd <my@sfogel.ru>
2023-02-23 12:07:46 +0300

    В лейбл добавлена строка "текущее значение:" для практики работы с NSAttributedString Добавлена кнопка Reset для сброса данных счётчика

--- Counter of Everything/ViewController.swift
+++ Counter of Everything/ViewController.swift
@@ -6,9 +6,31 @@ class ViewController: UIViewController {
     
     private var count = 0
     
+    private let firstAttribute: [NSAttributedString.Key: Any] = [.font: UIFont(name: "Arial", size: 24.0)! ]
+    private let secondAttribute: [NSAttributedString.Key: Any] = [.font: UIFont(name: "Arial", size: 140.0)! ]
+    
+    private func attributedLabel() {
+        let firstString = NSMutableAttributedString(string: "Текущее значение: \n", attributes: firstAttribute)
+        let secondString = NSAttributedString(string: "\(count)", attributes: secondAttribute)
+        firstString.append(secondString)
+       counterNumber.attributedText = firstString
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        attributedLabel()
+    }
+
+
     @IBAction private func buttonTapped(_ sender: Any) {
         count += 1
-        counterNumber.text = String(count)
+        attributedLabel()
     }
+
+    @IBAction private func ResetButton(_ sender: Any) {
+        count = 0
+        attributedLabel()
+    }
+        
 }
 
